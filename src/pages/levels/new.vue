328a01205ef9e89230de2456e6291790528d4916
Zhixing Zhang <me@neoto.xin>
2019-03-09 07:39:02 -0500

    Level file upload

---
+++ src/pages/levels/new.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div
+  section.section: .container.has-text-white
+    file-upload()
+</template>
+
+<script>
+import FileUpload from '~/components/levels/FileUpload'
+export default {
+  name: 'NewLevel',
+  components: {
+    FileUpload,
+  },
+  data() {
+    return {
+      levelFile: null
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
