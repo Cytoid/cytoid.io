7b65262d2b0e949c0967e17d1853d8a5e7e89482
Zhixing Zhang <me@neoto.xin>
2019-03-09 07:39:02 -0500

    Level file upload

---
+++ src/components/levels/FileUpload.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  b-upload(v-model="levelFile" drag-drop accept=".cytoidlevel")
+    .section: .content.has-text-centered
+      b-icon(icon="upload" size="is-large")
+      p Drop your files here or click to upload
+      progress.progress.is-small(v-if="uploadProgress" :value="uploadProgress" max="1")
+</template>
+
+<script>
+export default {
+  name: 'FileUpload',
+  data() {
+    return {
+      levelFile: null,
+      uploadProgress: null,
+    }
+  },
+  watch: {
+    async levelFile(newFile) {
+      if (!newFile) {
+        return
+      }
+      const uploadInfo = await this.$axios.post('/levels/packages').then(res => res.data)
+      await this.$axios.put(uploadInfo.uploadURL, newFile, {
+        headers: {
+          'Content-Type': 'application/zip'
+        },
+        onUploadProgress: (progressEvent) => {
+          this.uploadProgress = progressEvent.loaded / progressEvent.total
+        }
+      })
+      await this.$axios.post('/levels/packages', { key: uploadInfo.key })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
