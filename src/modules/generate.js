da1d5b69529a9e7862d73cdd13494ea3755fe7e0
Zhixing Zhang <me@neoto.xin>
2019-03-14 01:34:30 -0400

    Switch to Firebase Hosting

---
+++ src/modules/generate.js
@@ -0,0 +1,9 @@
+const blackList = [
+  '/levels',
+]
+module.exports = function () {
+  this.nuxt.hook('generate:extendRoutes', async routes => {
+    const routesToGenerate = routes.filter(page => !blackList.includes(page.route))
+    routes.splice(0, routes.length, ...routesToGenerate)
+  })
+}
