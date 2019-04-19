3be56d0756190592e4d5473e0d3c02496d030bc6
Zhixing Zhang <me@neoto.xin>
2019-04-19 02:02:57 -0400

    Nuxt Auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,6 @@
 export const state = () => ({
-  user: null
 })
 export const mutations = {
-  setUser(state, user) {
-    state.user = user
-  },
 }
 export const actions = {
-  nuxtServerInit({ commit }, { req }) {
-    if (req) {
-      // Obtain User Info from the session
-      const session = req.ctx.session
-      if (session && session.passport && session.passport.user) {
-        commit('setUser', req.ctx.session.passport.user)
-      } else {
-        commit('setUser', false)
-      }
-    }
-  },
-  logout({ commit }) {
-    return this.$axios.delete('/session')
-      .then(() => {
-        commit('setUser', null)
-      })
-  },
 }
