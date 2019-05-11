ff03ca670959585adf50c0aedd42c09b8abc36c5
TigerHix <tigerhix@gmail.com>
2019-05-12 02:23:15 +0800

    Add LevelCard

--- src/pages/levels/index.vue
+++ src/pages/levels/index.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
-    <level-card data-image="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Canyons</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </level-card>
-    <!--<a-card>
-      <div style="width: 100%; padding-top: 62.5%">
-        <img class="unselectable" style="border-radius: 4px; width: 100%; height: 100%; position: absolute; top: 0;"/>
-      </div>
-    </a-card>-->
+    <a-row type="flex" justify="center">
+      <a-col>
+        <level-card data-image="/images/Kreutz.png">
+          <div slot="title">
+            Kreutz
+          </div>
+          <div slot="uploader">
+            tigerhix
+          </div>
+        </level-card>
+      </a-col>
+      <a-col>
+        <level-card data-image="/images/Laeti.png">
+          <div slot="title">
+            Omnislash
+          </div>
+          <div slot="uploader">
+            suconh
+          </div>
+        </level-card>
+      </a-col>
+      <!--<a-card>
+        <div style="width: 100%; padding-top: 62.5%">
+          <img class="unselectable" style="border-radius: 4px; width: 100%; height: 100%; position: absolute; top: 0;"/>
+        </div>
+      </a-card>-->
+    </a-row>
   </div>
 </template>
 
 <script>
 import LevelCard from '@/components/level/LevelCard'
+
 export default {
   components: {
-    LevelCard
-  }
+    LevelCard,
+  },
 }
 </script>
