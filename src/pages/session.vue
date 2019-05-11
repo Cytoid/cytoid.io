4c2dda55d07a7883fcf52485d58ee002c45eb0c0
TigerHix <tigerhix@gmail.com>
2019-05-12 00:04:06 +0800

    Amend commit

--- src/pages/session.vue
+++ src/pages/session.vue
@@ -4,7 +4,10 @@
       <a-col :xs="{span: 0}" :lg="{span: 8}" />
       <a-col :xs="{span: 24}" :lg="{span: 16}" style="padding: 24px; max-width: 400px">
         <a-card :bordered="false" class="ele2" style="width: 100%;">
-          <login />
+          <div style="display:none">
+            <sign-in />
+          </div>
+          <sign-up />
         </a-card>
       </a-col>
     </a-row>
@@ -12,15 +15,15 @@
 </template>
 
 <script>
-import Login from '@/components/auth/Login'
+import SignIn from '@/components/session/SignIn'
+import SignUp from '@/components/session/SignUp'
 export default {
+  components: {
+    SignIn, SignUp
+  },
   data() {
     return {
-      currentSessionComponent: 'sign-in-form'
     }
-  },
-  components: {
-    Login,
   }
 }
 </script>
