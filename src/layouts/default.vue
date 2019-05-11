3db37f8a094e4a3768da36c865295bb86576854d
Neo <me@neoto.xin>
2019-05-11 22:53:56 +0800

    User Popup

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,44 @@
 <template lang="pug">
   div
-    cy-menu()
-      cy-menu-link-item(to="/" exact) Home
-      cy-menu-link-item(to="/levels") Levels
+    header.cy-navbar
+      cy-menu
+        cy-menu-link-item(to="/" exact) Home
+        cy-menu-link-item(to="/levels") Levels
+      .cy-navbar-end
+        a-popover(
+          trigger="click"
+          v-model="userPopoverVisible"
+          placement="bottomLeft"
+        )
+          div(slot="content") This is the content
+          a-button(type="primary" shape="circle" icon="user")
     nuxt
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userPopoverVisible: false,
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.cy-navbar {
+  ul.ant-menu-root {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  display: flex;
+  .cy-navbar-end {
+    justify-content: flex-end;
+    flex-grow: 3;
+    display: flex;
+    align-items: center;
+    padding-right: 2rem;
+  }
+}
+
+</style>
