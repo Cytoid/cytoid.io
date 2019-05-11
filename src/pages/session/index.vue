eba727c3b05f1c782bf6365aff51e01c97f10f99
TigerHix <tigerhix@gmail.com>
2019-05-11 23:00:51 +0800

    Sign in components WIP

---
+++ src/pages/session/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a-row type="flex" justify="space-around" align="middle">
+      <a-col :xs="{span: 0}" :lg="{span: 8}" />
+      <a-col :xs="{span: 24}" :lg="{span: 16}" style="padding: 24px; max-width: 400px">
+        <a-card :bordered="false" class="ele2" style="width: 100%;">
+          <component :is="currentSessionComponent"></component>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentSessionComponent: 'sign-in-form'
+    }
+  }
+}
+</script>
