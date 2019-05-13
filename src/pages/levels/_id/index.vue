b8e4cf79387e2761f69439016e0f72af56509a22
TigerHix <tigerhix@gmail.com>
2019-05-13 22:08:15 +0800

    More level info

--- src/pages/levels/_id/index.vue
+++ src/pages/levels/_id/index.vue
@@ -10,22 +10,40 @@
         Download (10.2MB)
       </a-button>
     </div>
-    <a-card>
-      <player-avatar></player-avatar>
-      <div style="margin-bottom: 32px;">
-        <a-rate :defaultValue="4.5" allowHalf />
-        4.5 (273)
-      </div>
-      <div style="margin-bottom: 32px;">
-        <a-tag v-for="tag in level.tags" :key="tag">
-          {{ tag }}
-        </a-tag>
-      </div>
-    </a-card>
+    <a-row :gutter="16">
+      <a-col :xs="24" :lg="8">
+        <a-card style="margin-bottom: 16px;">
+          <player-avatar style="margin-bottom: 16px;"></player-avatar>
+          <p>This is a test description!</p>
+          <!-- TODO: Marked -->
+          <p class="card-heading">Rating</p>
+          <div style="margin-bottom: 16px;">
+            <a-rate :defaultValue="4.5" allowHalf />
+            <span class="card-secondary-text">4.5 (273)</span>
+          </div>
+          <p class="card-heading">Tags</p>
+          <div style="margin-bottom: 16px;">
+            <a-tag v-for="tag in level.tags" :key="tag">
+              {{ tag }}
+            </a-tag>
+          </div>
+          <p class="card-heading">Last updated</p>
+          <div class="card-secondary-text" style="margin-bottom: 0px;">
+            {{ readableDate(level.modificationDate) }}
+          </div>
+        </a-card>
+      </a-col>
+      <a-col :xs="24" :lg="16">
+        <a-card>
+          Rankings
+        </a-card>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import DifficultyBadge from '@/components/level/DifficultyBadge'
 import PlayerAvatar from '@/components/player/PlayerAvatar'
 export default {
@@ -45,6 +63,11 @@ export default {
   },
   mounted() {
     this.$root.$emit('background', { source: this.level.bundle.background })
+  },
+  methods: {
+    readableDate: function (date) {
+      return moment(date).fromNow()
+    }
   }
 }
 </script>
