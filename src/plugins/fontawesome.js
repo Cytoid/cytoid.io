f1a43415b717b55e0b3675f83e994591c8731ec8
Neo <me@neoto.xin>
2019-05-15 21:41:33 +0800

    Tweaks

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -3,6 +3,8 @@ import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/pro-solid-svg-icons'
 
-config.autoAddCss = false
-library.add(fas)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+export default function () {
+  config.autoAddCss = false
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+  library.add(fas)
+}
