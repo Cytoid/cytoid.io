932190891ffd4f2cfede3db118f39f4505dc536d
Neo <me@neoto.xin>
2019-05-22 17:57:51 +0800

    Bulma Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+header.navbar(role="navigation")
+  .container
+    .navbar-brand
+      slot(name="brand")
+      .navbar-burger(@click="expanded = !expanded" :class="{ 'is-active': expanded }")
+        span
+        span
+        span
+      .navbar-append.is-brand
+        slot(name="append")
+    .navbar-menu(:class="{ 'is-active': expanded }")
+      .navbar-start
+        slot(name="start")
+      .navbar-end
+        slot(name="end")
+    .navbar-append.is-end
+      slot(name="append")
+</template>
+
+<script>
+export default {
+  data: () => ({
+    expanded: false,
+  })
+}
+</script>
+
+<style lang="scss">
+@import "~bulma/sass/components/navbar";
+.navbar-append {
+  display: flex;
+  align-items: stretch;
+  &.is-brand {
+    @include from($navbar-breakpoint) {
+      .navbar-item,
+      .navbar-link {
+        display: none
+      }
+    }
+  }
+  &.is-end {
+    @include until($navbar-breakpoint) {
+      .navbar-item,
+      .navbar-link {
+        display: none;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/me.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  a-row
+    a-col(:span="8")
+      a-menu
+        a-menu-item 菜单项
+        a-sub-menu(title="子菜单")
+          a-menu-item 子菜单项
+    a-col(:span="16")
+      nuxt
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
