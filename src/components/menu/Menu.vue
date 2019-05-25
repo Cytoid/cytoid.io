0c5267d8636481d2f35e1bed0b49a70e26e440b9
Neo <me@neoto.xin>
2019-05-25 15:05:56 +0800

    DifficultyBadge connected to API

--- src/components/menu/Menu.vue
+++
@@ -1,5 +0,0 @@
-<template functional>
-  <ul role="menu" class="ant-menu-horizontal ant-menu-root ant-menu-light">
-    <slot />
-  </ul>
-</template>
--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Antd from 'ant-design-vue/lib'
 
-import Menu from '../components/menu/Menu'
-import MenuLinkItem from '../components/menu/MenuLinkItem'
-
 export default function () {
   Vue.use(Antd)
-
-  Vue.component('cy-menu', Menu)
-  Vue.component('cy-menu-link-item', MenuLinkItem)
 }
