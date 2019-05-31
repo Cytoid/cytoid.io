e2c3f55e5d577617232188c71d8453febfe8dd30
TigerHix <tigerhix@gmail.com>
2019-05-30 23:10:53 -0400

    More styling

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,7 +9,8 @@
       template(slot="append")
         nuxt-link.navbar-item(v-if="$auth.loggedIn" to="/me") Profile
         .navbar-item(v-else)
-          nuxt-link.button.is-primary(to="/session/signin") Sign In
+          nuxt-link(to="/session/signin")
+            a-button(style="font-size: 14px;") Sign in
     nuxt
     footer.container.footer.has-text-centered
       ul
