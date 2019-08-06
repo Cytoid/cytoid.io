512d4eb63181c40e444bf11fc597105da70f3876
Zhixing Zhang <me@neoto.xin>
2019-08-06 14:28:52 +0800

    Remove shits

--- src/pages/session.vue
+++ src/pages/session.vue
@@ -4,36 +4,6 @@
       <a-col :xs="{span: 0}" :lg="{span: 8}" />
       <a-col :xs="{span: 24}" :lg="{span: 16}" style="padding: 24px; max-width: 400px">
         <a-card :bordered="false" class="ele2 session-card" style="width: 100%;">
-          <!-- TODO: Refactor this shit lol -->
-          <div
-            style="
-                            position: absolute;
-                            width: 100%;
-                            height: 100%;
-                            border-radius: 4px;
-                            background: linear-gradient(to left, #B06AB3, #4568DC);
-                            "
-          />
-          <div
-            style="
-                            position: absolute;
-                            bottom: 67%;
-                            width: 100%;
-                            height: 33%;
-                            background: linear-gradient(to top, hsla(226, 15%, 19%, 1), rgba(0,0,0,0) 100%);
-                            border-radius: 4px 4px 0 0;
-                            "
-          />
-          <div
-            style="
-                            position: absolute;
-                            bottom: 0;
-                            width: 100%;
-                            height: 67%;
-                            border-radius: 0 0 4px 4px;
-                            background: hsla(226, 15%, 19%, 1);
-                            "
-          />
           <nuxt-child style="position:relative;z-index:1;padding:24px;" />
         </a-card>
       </a-col>
@@ -51,5 +21,8 @@ export default {
 <style lang="less">
   .session-card .ant-card-body {
     padding: 0;
+  }
+  .session-card {
+    background: linear-gradient(to top, hsla(226, 15%, 19%, 1) 66%, rgba(0,0,0,0)), linear-gradient(to left, #B06AB3, #4568DC);
   }
 </style>
