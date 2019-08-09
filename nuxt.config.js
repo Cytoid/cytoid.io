a7b8a5c0a6b55a9213e5e1cb7bc8f2c52cf75262
Zhixing Zhang <me@neoto.xin>
2019-08-09 11:18:21 +0800

    Fix Dependency Issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,8 @@ module.exports = {
     script: [
       { src: 'https://www.googletagmanager.com/gtag/js?id=' + config.get('analyticsCode'), async: true },
       { innerHTML: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config','${config.get('analyticsCode')}');` }
-    ]
+    ],
+    __dangerouslyDisableSanitizers: ['script']
   },
   /*
   ** Customize the progress-bar color
